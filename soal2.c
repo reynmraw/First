78b72f71b5e8ad199d5e54c748d7862610d9600d
stradzzz <33017938+stradzzz@users.noreply.github.com>
2017-10-25 17:42:35 +0700

    belom jadi (3)

--- soal2.c
+++ soal2.c
@@ -12,16 +12,18 @@ int bom[17], bom2[17];
 int pasang;
 int f,g;
 int main1=1, main2=0;
+int tebak1=0, tebak2=0;
+int poin1=0, poin2=0;
+
 void* playandcount(void *arg)
 {
     unsigned long i=0;
     pthread_t id=pthread_self();
 
+
+//THREEEAEADDD1111
     if(pthread_equal(id,tid[0]))
     {
-	printf ("masukkan nama pemain 1:\n");
-	scanf ("%c",&n1);
-
        	while (main1=1)
        	{
           	printf("letakkan bomb pada lubang 1-16\n");
@@ -33,13 +35,33 @@ void* playandcount(void *arg)
 				scanf ("%d", &f);
 				bom[f]=1;
 			}
+
 		}
 		main1=0;
-		main2=1;
+		tebak2=1;
 	}
+
+        while (tebak1=1);
+        {
+                printf ("tebak bom1");
+                for (int r=0;r<4;r++){
+                        scanf ("%d", &teb2);
+                        if (bom1[teb2]==1){
+                                poin1+=1;
+                        }
+                        else if (bom1[teb2]==0){
+                                poin2+=1;
+                        }
+                }
+	tebak1=0;
+	}
+
 	exit(0);
     }
 
+
+
+//THREADDDD2
 
     else if(pthread_equal(id,tid[1]))
     {
@@ -51,15 +73,36 @@ void* playandcount(void *arg)
 			scanf ("%d, &pasang);
 			if (pasang = 1){
 				printf ("pasang dimana");
-				scanf ("%d, &g);
+				scanf ("%d", &g);
 				bom2[g]=1;
 			}
 		}
 		main2=0;
-		main1=1;
 	}
-	exit(0);
+
+
+
+	while (tebak2=1);
+	{
+		printf ("tebak bom1");
+		for (int r=0;r<4;r++){
+			scanf ("%d", &teb2);
+			if (bom1[teb2]==1){
+				poin1+=1;
+			}
+			else if (bom1[teb2]==0){
+				poin2+=1;
+			}
+		}
+	tebak2=0;
+	main2=1;
+	}
     }
+
+
+
+
+
 
     if(pthread_equal(id,tid[2]))
     {
