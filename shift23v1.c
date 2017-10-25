433c54a00fa8efd9efe4b7c8f3fd13dd3ecb3e93
stradzzz <33017938+stradzzz@users.noreply.github.com>
2017-10-25 16:20:31 +0700

    NOMER 3. DONE.

--- shift23v1.c
+++ shift23v1.c
@@ -1,83 +1,86 @@
+
 #include<stdio.h>
 #include<string.h>
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<stdlib.h>
-/*******************************************************
-*compile dengan cara gcc -pthread -o [output] input.c *
-*******************************************************/
 
-pthread_t tid[3];//inisialisasi array untuk menampung thread dalam kasusu ini ada 2 thread
+pthread_t tid[3];
 int ikan=1,kepiting=1;
-int iter=100;
-int jir=100;//inisialisasi jumlah looping
+int statlohan=100;
+int statkep=100;
 int pilihan;
+
 void* playandcount(void *arg)
 {
     unsigned long i=0;
     pthread_t id=pthread_self();
-    
-    if(pthread_equal(id,tid[0]))//thread untuk menjalankan counter
+
+    if(pthread_equal(id,tid[0]))
     {
-	
-	
-       		 
-       		 for(iter;iter>0;)
-       	        {
-           	 	printf("\n nyawa lohan %i",iter);
-           	 	fflush(stdout);
-           	 	sleep(1);
-		 	iter-=15;
-	        }
-	
-	printf("\nIkannya mati oi");
+       	for(statlohan;statlohan>0;)
+       	{
+          	printf("nyawa lohan %i\n",statlohan);
+           	fflush(stdout);
+           	sleep(10);
+		statlohan-=15;
+	}
+
+	printf("\nRIP Lohan");
 	ikan=0;
 	exit(0);
     }
+
     else if(pthread_equal(id,tid[1]))
     {
-       
-		
-		for(jir;jir>0;)
-		{
-			printf("\n nyawa kepiting %i",jir);
-			fflush(stdout);
-			sleep(2);
-			jir-=10;
-		}
-	
-	printf("\nKepitingnya mati Dil");
+	for(statkep;statkep>0;)
+	{
+		printf("			nyawa kepiting %i\n",statkep);
+		fflush(stdout);
+		sleep(20);
+		statkep-=10;
+	}
+
+	printf("\nRIP Kepiting");
 	kepiting=0;
 	exit(0);
     }
- 
+
     if(pthread_equal(id,tid[2]))
     {
-	while(jir<=100 || iter<=100)
+	while(statlohan<=100 && statkep<=100)
 	{
-		printf("1.Kasih makan lohan\n2.Kasih makan kepiting\n");
 		scanf("%d",&pilihan);
-		if(pilihan==1) iter+=10;
-		else if(pilihan==2) jir+=10;
-		
+		if(pilihan==1) 
+		{
+			statlohan+=10;
+			printf ("nyawa lohan menjadi %i\n",statlohan);
+		}
+		else if(pilihan==2) 
+		{
+			statkep+=10;
+			printf ("			nyawa kepiting menjadi %i\n", statkep);
+		}
 	}
-	printf("Kolam Overload");
+	printf("Kekenyangan gan\n");
 	exit(0);
     }
 return NULL;
 }
+
 int main(void)
 {
-    int i=0;
-    int err;
-    while(i<3)//looping membuat thread 2x
-    {
-        err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);//membuat thread
-        i++;
-    }
-    pthread_join(tid[0],NULL);
-    pthread_join(tid[1],NULL);
-    pthread_join(tid[2],NULL);
-    return 0;
+	int i=0;
+	int err;
+	printf ("\nOPSI 	:\n 1. Kasih makan lohan\n 2. Kasih makan kepiting\n\n\n");
+	while(i<3)
+    	{
+        	err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);
+        	i++;
+    	}
+    	pthread_join(tid[0],NULL);
+    	pthread_join(tid[1],NULL);
+   	pthread_join(tid[2],NULL);
+    	return 0;
 }
