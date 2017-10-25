35f039d2b613a99d6d75338ff4ed714b62dcbcc7
stradzzz <33017938+stradzzz@users.noreply.github.com>
2017-10-25 17:07:46 +0700

    belom jadi

---
+++ soal2.c
@@ -0,0 +1,93 @@
+
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+pthread_t tid[3];
+char n1,n2;
+int bom [16];
+int pasang;
+int f;
+
+void* playandcount(void *arg)
+{
+    unsigned long i=0;
+    pthread_t id=pthread_self();
+
+    if(pthread_equal(id,tid[0]))
+    {
+	printf ("masukkan nama pemain 1:\n");
+	scanf ("%c",&n1);
+
+       	while (main1==1)
+       	{
+          	printf("letakkan bomb pada lubang 1-16\n");
+		for (int i=0;i<4;i++){
+			printf ("masang?");
+			scanf ("%d",&pasang);
+			if (pasang == 1){
+				printf ("pasang dimana")
+				scanf ("%d", &f);
+				bom[f]=1;
+		main1==0;
+		main2==1;
+	};
+	exit(0);
+    }
+
+
+    else if(pthread_equal(id,tid[1]))
+    {
+	for(statkep;statkep>0;)
+	{
+		printf("			nyawa kepiting %i\n",statkep);
+		fflush(stdout);
+		sleep(20);
+		statkep-=10;
+	}
+
+	printf("\nRIP Kepiting");
+	kepiting=0;
+	exit(0);
+    }
+
+    if(pthread_equal(id,tid[2]))
+    {
+	while(statlohan<=100 && statkep<=100)
+	{
+		scanf("%d",&pilihan);
+		if(pilihan==1) 
+		{
+			statlohan+=10;
+			printf ("nyawa lohan menjadi %i\n",statlohan);
+		}
+		else if(pilihan==2) 
+		{
+			statkep+=10;
+			printf ("			nyawa kepiting menjadi %i\n", statkep);
+		}
+	}
+	printf("Kekenyangan gan\n");
+	exit(0);
+    }
+return NULL;
+}
+
+int main(void)
+{
+	int i=0;
+	int err;
+	printf ("\nOPSI 	:\n 1. Kasih makan lohan\n 2. Kasih makan kepiting\n\n\n");
+	while(i<3)
+    	{
+        	err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);
+        	i++;
+    	}
+    	pthread_join(tid[0],NULL);
+    	pthread_join(tid[1],NULL);
+   	pthread_join(tid[2],NULL);
+    	return 0;
+}
